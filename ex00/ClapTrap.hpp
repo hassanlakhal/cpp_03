be6b1d7c7ae196884c7a371930a4f401a02a51db
Hassan Lakhal <hlakhal-@e1r6p8.1337.ma>
2023-10-17 16:54:27 +0100

    add ex00

---
+++ ex00/ClapTrap.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Claptrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hlakhal- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/12 11:21:24 by hlakhal-          #+#    #+#             */
+/*   Updated: 2023/10/12 12:33:36 by hlakhal-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<iostream>
+
+class ClapTrap
+{
+private:
+   std::string name;
+   unsigned int hitPoint;
+   unsigned int energyPoints;
+   unsigned int attackDamage;
+public:
+      ClapTrap();
+      ClapTrap(std::string str);
+      ClapTrap& operator = (ClapTrap &other);
+      ClapTrap(ClapTrap &other);
+      ~ClapTrap();
+      void attack(const std::string& target);
+      void takeDamage(unsigned int amount);
+      void beRepaired(unsigned int amount);
+ };
+
+
---
+++ ex00/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hlakhal- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/12 11:03:36 by hlakhal-          #+#    #+#             */
+/*   Updated: 2023/10/12 15:55:02 by hlakhal-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"ClapTrap.hpp"
+
+int main() 
+{
+    ClapTrap claptrap("CL4P-TP");
+
+    // claptrap.takeDamage(2);
+    claptrap.attack("Enemy");
+    claptrap.takeDamage(15);
+    // claptrap.beRepaired(20);
+    claptrap.takeDamage(20);
+    claptrap.attack("Enemy");
+    return 0;
+}
