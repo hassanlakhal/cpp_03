ef35bbb0c00fb1ffb9eac9195c37d87da6edc2c5
Hassan Lakhal <hlakhal-@e1r4p8.1337.ma>
2023-10-21 23:05:20 +0100

    fixed error in Diamond class

--- ex03/FragTrap.hpp
+++ ex03/FragTrap.hpp
@@ -6,12 +6,9 @@
 /*   By: hlakhal- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 16:50:59 by hlakhal-          #+#    #+#             */
-/*   Updated: 2023/10/21 19:45:55 by hlakhal-         ###   ########.fr       */
+/*   Updated: 2023/10/21 23:03:31 by hlakhal-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// #ifndef FRAGTRAP_HPP
-// #ifdef FRAGTRAP_HPP
 
 #include"ScavTrap.hpp"
 
@@ -26,4 +23,3 @@ class FragTrap: virtual public ClapTrap
         FragTrap& operator=(const FragTrap& other);
         ~FragTrap();
 };
-// #endif
